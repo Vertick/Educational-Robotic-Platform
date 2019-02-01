d536c797a91011f9cc1c3696075eaa82a9c04799
Pedro <pedro.plaza@hotmail.com>
2019-02-01 17:36:53 +0100

    Add files via upload

--- Software/Arduino_4/Arduino_4.ino
+++ Software/Arduino_4/Arduino_4.ino
@@ -3,17 +3,8 @@
 /**************************************************************************************************/
 /* Author name: Pedro Plaza                                                                       */
 /**************************************************************************************************/
-/* Initial version 1 - 09/12/2017                                                                 */
+/* Second version 1.1 - 31/01/2019                                                                */
 /**************************************************************************************************/
-
-#include <Servo.h> 
- 
-Servo PWM_1;
-Servo PWM_2;
-Servo PWM_3;
-Servo PWM_4;
-Servo PWM_5;
-Servo PWM_6;
 
 const int INPUT_1 =  2;// INPUT_1 set to D2 pin
 const int INPUT_2 =  4;// INPUT_2 set to D4 pin
@@ -23,8 +14,6 @@ const int OUTPUT_2 =  12;// OUTPUT_2 set to D12 pin
 const int OUTPUT_3 =  13;// OUTPUT_3 set to D13 pin
 
 const int ARDUINO_ID = 4;// Arduino identifier
-const int SERVO_OFFSET = 30;// Offset for servo rotation angle
-const int SERVO_STEP = 5;// Step for servo rotation angle
 int initialized = 0; // Variable to detect the start-up
 int seconds_counter = 0; // Variable to detect the start-up
  
@@ -32,13 +21,6 @@ void setup()
 {
   // Initialize serial at 9600 baudrate:
   Serial.begin(115200);
-  // Initialize PWM ports
-  PWM_1.attach(3, 771, 1798);
-  PWM_2.attach(5, 771, 1798);
-  PWM_3.attach(6, 771, 1798);
-  PWM_4.attach(9, 771, 1798);
-  PWM_5.attach(10, 771, 1798);
-  PWM_6.attach(11, 771, 1798);
   // Set the input digital pins
   pinMode(INPUT_1, INPUT);
   pinMode(INPUT_2, INPUT);
@@ -51,13 +33,6 @@ void setup()
  
 void loop() 
 { 
-
-  PWM_1.write((ARDUINO_ID)*SERVO_OFFSET+SERVO_STEP*seconds_counter+1);
-  PWM_2.write((ARDUINO_ID)*SERVO_OFFSET+SERVO_STEP*seconds_counter+2);
-  PWM_3.write((ARDUINO_ID)*SERVO_OFFSET+SERVO_STEP*seconds_counter+3);
-  PWM_4.write((ARDUINO_ID)*SERVO_OFFSET+SERVO_STEP*seconds_counter+4);
-  PWM_5.write((ARDUINO_ID)*SERVO_OFFSET+SERVO_STEP*seconds_counter+5);
-  PWM_6.write((ARDUINO_ID)*SERVO_OFFSET+SERVO_STEP*seconds_counter+6);
 
   if (initialized == 0)
   {
@@ -118,4 +93,3 @@ void loop()
   }
 
 } 
-
